cf27804070a54428b3ca65bca2c096eccc6d0a55
eluck <mail@eluck.me>
2015-07-19 19:36:22 +0300

    Use spaces for identation in the tests

--- tests/00.trueisnotfalse.js
+++ tests/00.trueisnotfalse.js
@@ -4,62 +4,62 @@ var assert = require("assert"),
 
 
 describe('check load balancing directives parsing', function() {
-	it ('should parse "least_conn" method', function() {
-		var result = parser.parse('' +
-			'upstream upst {' +
-			'  least_conn;' +
-			'  server 8.8.8.8;' +
-			'}');
-		assert.equal(result[0][2][0][0], "least_conn");
-	});
+  it ('should parse "least_conn" method', function() {
+    var result = parser.parse('' +
+      'upstream upst {' +
+      '  least_conn;' +
+      '  server 8.8.8.8;' +
+      '}');
+    assert.equal(result[0][2][0][0], "least_conn");
+  });
 
-	it ('should parse "ip_hash" method', function() {
-		var result = parser.parse('' +
-			'upstream upst {' +
-			'  ip_hash;' +
-			'  server 8.8.8.8;' +
-			'}');
-		assert.equal(result[0][2][0][0], "ip_hash");
-	});
+  it ('should parse "ip_hash" method', function() {
+    var result = parser.parse('' +
+      'upstream upst {' +
+      '  ip_hash;' +
+      '  server 8.8.8.8;' +
+      '}');
+    assert.equal(result[0][2][0][0], "ip_hash");
+  });
 
-	it ('should work fine with comments in the end of line', function() {
-		var result = parser.parse('' +
-			'upstream upst {' +
-			'  ip_hash; #comment\n' +
-			'  server 8.8.8.8;' +
-			'}');
-		assert.equal(result[0][2][0][0], "ip_hash");
-	});
+  it ('should work fine with comments in the end of line', function() {
+    var result = parser.parse('' +
+      'upstream upst {' +
+      '  ip_hash; #comment\n' +
+      '  server 8.8.8.8;' +
+      '}');
+    assert.equal(result[0][2][0][0], "ip_hash");
+  });
 
-	it ('should not allow setting load balancing method twice', function() {
-		assert.throws( function() {
+  it ('should not allow setting load balancing method twice', function() {
+    assert.throws( function() {
       parser.parse('' +
         'upstream upst {' +
         '  ip_hash;' +
         '  ip_hash;' +
         '  server 8.8.8.8;' +
         '}');
-		});
-	});
+    });
+  });
 
-	it ('should not allow redefining load balancing method in the same section', function() {
-		assert.throws( function() {
-			parser.parse('' +
-				'upstream upst {' +
-				'  ip_hash;' +
-				'  least_conn;' +
-				'  server 8.8.8.8;' +
-				'}');
-		});
-	});
+  it ('should not allow redefining load balancing method in the same section', function() {
+    assert.throws( function() {
+      parser.parse('' +
+        'upstream upst {' +
+        '  ip_hash;' +
+        '  least_conn;' +
+        '  server 8.8.8.8;' +
+        '}');
+    });
+  });
 
-	it ('should not allow setting load balancing method in the middle of upstream section', function() {
-		assert.throws( function() {
-			parser.parse('' +
-				'upstream upst {' +
-				'  server 8.8.8.8;' +
-				'  ip_hash;' +
-				'}');
-		});
-	});
+  it ('should not allow setting load balancing method in the middle of upstream section', function() {
+    assert.throws( function() {
+      parser.parse('' +
+        'upstream upst {' +
+        '  server 8.8.8.8;' +
+        '  ip_hash;' +
+        '}');
+    });
+  });
 });
